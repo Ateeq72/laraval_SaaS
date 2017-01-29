9529ef1e22bc4b586c7a86fff2b3302c76704f13
Ateeq Ahmed <ahmedateeq64@gmail.com>
2017-01-29 15:57:22 +0530

    Add logic to Profile Update Screen

--- app/Http/Controllers/HALAteeqControllers/ProfileController.php
+++ app/Http/Controllers/HALAteeqControllers/ProfileController.php
@@ -10,11 +10,17 @@ namespace App\Http\Controllers\HALAteeqControllers;
 
 
 use App\Http\Controllers\Controller;
+use App\Tenants;
 use App\User;
+use App\UserMultiTenant;
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Input;
 
 class ProfileController extends Controller
 {
+    protected $redirectTo = '/';
+
     public function __construct()
     {
         $this->middleware('auth');
@@ -24,6 +30,16 @@ class ProfileController extends Controller
     {
         $data = self::getProfileData();
         return view('freelancer.profile')->with('data',$data);
+    }
+
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'name' => 'required|max:255',
+            'email' => 'required|email|max:255|unique:users',
+            'password' => 'required|min:6|confirmed',
+            'tenant_name' => 'required|max:255',
+        ]);
     }
 
     public function getProfileData(){
@@ -40,26 +56,71 @@ class ProfileController extends Controller
 
         $userDataArray['user_name'] = isset($userData->user_name) && $userData->user_name != null ? $userData->user_name : '';
         $userDataArray['user_email'] = isset($userData->user_email) && $userData->user_email != null ? $userData->user_email : '';
-        $userDataArray['user_tenant'] = isset($userData->user_tenant) && $userData->user_tenant != null ? $userData->user_tenant : '';
+        $userDataArray['user_tenant'] = isset($userData->tenant_name) && $userData->tenant_name != null ? $userData->tenant_name : '';
 
         return $userDataArray;
     }
 
-    protected function updateProfile(array $data)
+    protected function updateProfile()
     {
+        $data = Input::all();
+
         if(Auth::check()) {
-            $user = User::update(array(
-                'name' => $data['name'],
-                'email' => $data['email']))
-                ->where('id',Auth::user()->id);
+            $curUserId = Auth::user()->id;
+            $user = User::where('id',$curUserId)->first();
+            $user->name = $data['name'];
+            $user->email = $data['email'];
 
             if($data['password'] != 'NOCHANGE'){
-                $user = User::update(array(
-                    'password' => bcrypt($data['password'])))
-                    ->where('id',Auth::user()->id);
+            $user->password = bcrypt($data['password']);
             }
 
+            $user->save();
+
+            $userTenant = UserMultiTenant::where('user_id',$curUserId)->first();
+
+            $tenant = Tenants::where('tenant_name',$data['tenant_name'])->first();
+
+            if($userTenant != null && $tenant != null){
+                $userTenant->user_id = $curUserId;
+                $userTenant->tenant_id = $tenant->id;
+
+                $userTenant->save();
+            }
+            else if($userTenant == null && $tenant != null){
+                $userTenant = new UserMultiTenant();
+
+                $userTenant->user_id = $curUserId;
+                $userTenant->tenant_id = $tenant->id;
+                $userTenant->save();
+            }
+            else if($userTenant != null && $tenant == null)
+            {
+                $tenant = new Tenants();
+                $tenant->tenant_name = $data['tenant_name'];
+                $tenant->tenant_paid = 'N';
+                $tenant->save();
+
+                $userTenant->user_id = $curUserId;
+                $userTenant->tenant_id = $tenant->id;
+                $userTenant->save();
+            }
+            else if ($userTenant == null && $tenant == null){
+
+                $tenant = new Tenants();
+                $tenant->tenant_name = $data['tenant_name'];
+                $tenant->tenant_paid = 'N';
+                $tenant->save();
+
+                $userTenant = new UserMultiTenant();
+                $userTenant->user_id = $curUserId;
+                $userTenant->tenant_id = $tenant->id;
+                $userTenant->save();
+
+            }
+
+        return redirect('/profile');
         }
-        return '';
+        return 'Failure';
     }
 }
